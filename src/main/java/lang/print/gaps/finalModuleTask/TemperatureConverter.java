31882514bdd1cb5a084ea8ae21e25702e5a79cd1
Vu <nguyentuanvu9x@gmail.com>
2023-08-16 11:09:39 +0300

    Adding: TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float fahrenheit = (temperatureCelsius * 9 / 5.0f) + 32;
+        System.out.println(fahrenheit);
     }
 }
