a14aa5e07e1f8cebac60dec7a4b3adc9070258be
Vu <nguyentuanvu9x@gmail.com>
2023-08-16 10:53:41 +0300

    Adding: NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
-    public void printIsPositive(int number) {
-
+    public static void printIsPositive(int number) {
+        System.out.println(number > 0);
     }
 }
